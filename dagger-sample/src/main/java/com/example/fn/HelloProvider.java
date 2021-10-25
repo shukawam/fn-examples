8fce89ae24621495a85f6e4ea4e52895f4d992eb
shukawam <shukawam@gmail.com>
2021-10-25 23:06:30 +0900

    WIP: dagger-sample

--- dagger-sample/src/main/java/com/example/fn/HelloProvider.java
+++ dagger-sample/src/main/java/com/example/fn/HelloProvider.java
@@ -1,23 +1,12 @@
 package com.example.fn;
 
-import dagger.Component;
-
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class HelloProvider {
 
-    private final HelloLogger logger;
-
-    @Inject
-    public HelloProvider(HelloLogger logger) {
-        this.logger = logger;
-    }
-
     public String say(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
-        logger.log("Inside Java Hello World function");
+        String name = (input == null || input.isEmpty()) ? "world" : input;
         return "Hello, " + name + "!";
     }
 }
