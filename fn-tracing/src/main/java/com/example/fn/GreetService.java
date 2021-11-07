32b93580c32396439271bbb3fa16de99faa6a08f
shukawam <shukawam@gmail.com>
2021-11-07 23:18:21 +0900

    Added thread.sleep

--- fn-tracing/src/main/java/com/example/fn/GreetService.java
+++ fn-tracing/src/main/java/com/example/fn/GreetService.java
@@ -19,13 +19,28 @@ public class GreetService {
 
     public void method1() {
         logger.log(Level.INFO, "Inside Method1 function");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void method2() {
         logger.log(Level.INFO, "Inside Method2 function");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void method3() {
         logger.log(Level.INFO, "Inside Method3 function");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
