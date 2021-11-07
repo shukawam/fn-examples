bf382cff266ac48898a1adfcbb11adbb369d2f06
shukawam <shukawam@gmail.com>
2021-11-07 21:52:35 +0900

    Added debug log

--- fn-tracing/src/main/java/com/example/fn/interceptor/ApmTraceInterceptor.java
+++ fn-tracing/src/main/java/com/example/fn/interceptor/ApmTraceInterceptor.java
@@ -47,18 +47,24 @@ public class ApmTraceInterceptor implements MethodInterceptor {
     private void initializeZipkin(TracingContext tracingContext) {
         logger.log(Level.INFO, "Initializing the variables");
         apmUrl = tracingContext.getTraceCollectorURL();
+        logger.log(Level.INFO, "Apm URL: " + apmUrl);
         sender = URLConnectionSender.create(apmUrl);
+        logger.log(Level.INFO, "Sender: " + sender);
         zipkinSpanHandler = AsyncZipkinSpanHandler.create(sender);
+        logger.log(Level.INFO, "ZipkinSpanHandler: " + zipkinSpanHandler);
         tracing = Tracing.newBuilder()
                 .localServiceName(tracingContext.getServiceName())
                 .addSpanHandler(zipkinSpanHandler)
                 .build();
+        logger.log(Level.INFO, "Tracing: " + tracing);
         tracer = tracing.tracer();
+        logger.log(Level.INFO, "Tracer: " + tracer);
         tracing.setNoop(!tracingContext.isTracingEnabled());
         traceContext = TraceContext.newBuilder()
                 .traceId(IdConversion.convertToLong(tracingContext.getTraceId()))
                 .spanId(IdConversion.convertToLong(tracingContext.getSpanId()))
                 .sampled(tracingContext.isSampled())
                 .build();
+        logger.log(Level.INFO, "TraceContext: " + traceContext);
     }
 }
