22d499036c9295b87a581f08090a7481ab639548
shukawam <shukawam@gmail.com>
2021-11-08 11:01:36 +0900

    Added logging

--- fn-tracing/src/main/java/com/example/fn/HelloFunction.java
+++ fn-tracing/src/main/java/com/example/fn/HelloFunction.java
@@ -6,6 +6,8 @@ import brave.Tracing;
 import brave.propagation.TraceContext;
 import com.example.fn.annotations.ApmTrace;
 import com.example.fn.interceptor.ApmTraceInterceptor;
+import com.fnproject.fn.api.Headers;
+import com.fnproject.fn.api.InvocationContext;
 import com.fnproject.fn.api.tracing.TracingContext;
 import com.github.kristofa.brave.IdConversion;
 import com.google.inject.AbstractModule;
@@ -31,17 +33,9 @@ public class HelloFunction {
     @Inject
     private GreetService greetService;
 
-    public String handleRequest(String input, TracingContext tracingContext) {
-        logger.log(Level.INFO, "tracingContext.functionName: " + tracingContext.getFunctionName());
-        logger.log(Level.INFO, "tracingContext.appName: " + tracingContext.getAppName());
-        logger.log(Level.INFO, "tracingContext.traceId: " + tracingContext.getTraceId());
-        logger.log(Level.INFO, "tracingContext.serviceName: " + tracingContext.getServiceName());
-        logger.log(Level.INFO, "tracingContext.spanId: " + tracingContext.getSpanId());
-        logger.log(Level.INFO, "tracingContext.flags: " + tracingContext.getFlags());
-        logger.log(Level.INFO, "tracingContext.parentSpanId: " + tracingContext.getParentSpanId());
-        logger.log(Level.INFO, "tracingContext.traceCollectorUrl: " + tracingContext.getTraceCollectorURL());
-        logger.log(Level.INFO, "tracingContext.tracingEnabled: " + tracingContext.isTracingEnabled());
-        logger.log(Level.INFO, "tracingContext.sampled: " + tracingContext.isSampled());
+    public String handleRequest(String input, TracingContext tracingContext, InvocationContext invocationContext) {
+        loggingFunctionsHeader(invocationContext);
+        loggingTracingContext(tracingContext);
         try {
             initializeZipkin(tracingContext);
             // Start a new tracer or a span within an existing trace representing an operation.
@@ -94,4 +88,22 @@ public class HelloFunction {
                 .build();
     }
 
+    private void loggingTracingContext(TracingContext tracingContext) {
+        logger.log(Level.INFO, "tracingContext.functionName: " + tracingContext.getFunctionName());
+        logger.log(Level.INFO, "tracingContext.appName: " + tracingContext.getAppName());
+        logger.log(Level.INFO, "tracingContext.traceId: " + tracingContext.getTraceId());
+        logger.log(Level.INFO, "tracingContext.serviceName: " + tracingContext.getServiceName());
+        logger.log(Level.INFO, "tracingContext.spanId: " + tracingContext.getSpanId());
+        logger.log(Level.INFO, "tracingContext.flags: " + tracingContext.getFlags());
+        logger.log(Level.INFO, "tracingContext.parentSpanId: " + tracingContext.getParentSpanId());
+        logger.log(Level.INFO, "tracingContext.traceCollectorUrl: " + tracingContext.getTraceCollectorURL());
+        logger.log(Level.INFO, "tracingContext.tracingEnabled: " + tracingContext.isTracingEnabled());
+        logger.log(Level.INFO, "tracingContext.sampled: " + tracingContext.isSampled());
+    }
+
+    private void loggingFunctionsHeader(InvocationContext invocationContext) {
+        invocationContext.getRequestHeaders().asMap().forEach((key, value) -> {
+            logger.log(Level.INFO, "key: " + key + " value: " + value);
+        });
+    }
 }
