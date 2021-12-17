f87d1bad1b2c385fea7737acb35a61b336b78a3e
shukawam <shukawam@gmail.com>
2021-12-17 15:17:26 +0900

    Added header logging feature

--- print-header/src/main/java/com/example/fn/HelloFunction.java
+++ print-header/src/main/java/com/example/fn/HelloFunction.java
@@ -1,12 +1,24 @@
 package com.example.fn;
 
+import com.fnproject.fn.api.InputEvent;
+
+import java.util.logging.Logger;
+
 public class HelloFunction {
+    private static final Logger logger = Logger.getLogger(HelloFunction.class.getName());
 
-    public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
-
-        System.out.println("Inside Java Hello World function"); 
+    public String handleRequest(String input, InputEvent inputEvent) {
+        String name = (input == null || input.isEmpty()) ? "world" : input;
+        // simple header logging
+        printHeader(inputEvent);
+        System.out.println("Inside Java Hello World function");
         return "Hello, " + name + "!";
+    }
+
+    private static void printHeader(InputEvent inputEvent) {
+        inputEvent.getHeaders().asMap().forEach((key, value) -> {
+            logger.info(String.format("Header %s: %s", key, value));
+        });
     }
 
 }
