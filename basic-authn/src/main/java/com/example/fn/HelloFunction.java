d0e9ff216ea47f14059063d083985ecea0d015a8
shukawam <shukawam@gmail.com>
2022-05-18 01:59:26 +0000

    Added adm resources

--- basic-authn/src/main/java/com/example/fn/HelloFunction.java
+++ basic-authn/src/main/java/com/example/fn/HelloFunction.java
@@ -7,9 +7,7 @@ import com.fnproject.fn.api.FnConfiguration;
 import java.nio.charset.StandardCharsets;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
 
 public class HelloFunction {
     private static final String TYPE = "TOKEN";
@@ -17,8 +15,6 @@ public class HelloFunction {
     private static final DateTimeFormatter ISO8601 = DateTimeFormatter.ISO_DATE_TIME;
 
     // Simple Http-basic authentication.
-    private static final String GUEST_USER = "guest";
-    private static final String GUEST_PASSWORD = "password#123";
     private static final String ADMIN_USER = "admin";
     private static final String ADMIN_PASSWORD = "password#123";
 
@@ -32,7 +28,7 @@ public class HelloFunction {
         if (!TYPE.equals(authorizerRequest.getType()) || authorizerRequest.getType() == null) {
             var response = new AuthorizerResponse();
             response.setActive(false);
-            response.setWwwAuthenticate("Basic realm=\"Input type is invalid.\"");
+            response.setWwwAuthenticate("Basic realm=\"foo\"");
             return response;
         }
         if ("".equals(authorizerRequest.getToken()) ||
@@ -40,21 +36,24 @@ public class HelloFunction {
                 !authorizerRequest.getToken().startsWith(TOKEN_PREFIX)) {
             var response = new AuthorizerResponse();
             response.setActive(false);
-            response.setWwwAuthenticate("Basic realm=\"Input token is invalid.\"");
+            response.setWwwAuthenticate("Basic realm=\"foo\"");
             return response;
         }
         // token validation
-        var guestCredential = new String(Base64.getUrlEncoder().encode(String.format("%s:%s", GUEST_USER, GUEST_PASSWORD)
-                .getBytes(StandardCharsets.UTF_8)));
         var adminCredential = new String(Base64.getUrlEncoder().encode(String.format("%s:%s", ADMIN_USER, ADMIN_PASSWORD)
                 .getBytes(StandardCharsets.UTF_8)));
         var inputToken = authorizerRequest.getToken().substring(TOKEN_PREFIX.length());
-        if (!guestCredential.equals(inputToken) && !adminCredential.equals(inputToken)) {
+        if (!adminCredential.equals(inputToken)) {
             var response = new AuthorizerResponse();
             response.setActive(false);
-            response.setWwwAuthenticate("Basic realm=\"Username or password is wrong.\"");
+            response.setWwwAuthenticate("Basic realm=\"foo\"");
             return response;
         }
+        var response = createResponse(inputToken);
+        return response;
+    }
+
+    private AuthorizerResponse createResponse(String inputToken) {
         var response = new AuthorizerResponse();
         response.setActive(true);
         var user = new String(Base64.getUrlDecoder().decode(inputToken)).split(":")[0];
